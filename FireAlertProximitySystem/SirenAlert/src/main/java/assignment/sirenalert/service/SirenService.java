1ded96eee941735d9099828499e3589de5ef5a08
codebymks <228756637+codebymks@users.noreply.github.com>
2026-03-23 16:33:32 +0100

    Clean up comments in SirenService interface
    Removed unnecessary comments from SirenService interface methods.

--- FireAlertProximitySystem/SirenAlert/src/main/java/assignment/sirenalert/service/SirenService.java
+++ FireAlertProximitySystem/SirenAlert/src/main/java/assignment/sirenalert/service/SirenService.java
@@ -6,18 +6,9 @@ import java.util.List;
 
 public interface SirenService {
 
-    //liste af sirens
     List<Siren> getAllSiren();
-
-    //liste med specifik id
     Siren getSirenById(int id);
-
-    //lav en sirene
     Siren createSiren(Siren siren);
-
-    //opdater sirene
     Siren updateSiren(int id, Siren updatedSiren);
-
-    //slet sirene
     void deleteSiren(int id);
 }
